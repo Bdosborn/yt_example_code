d6df1b1813a4bbb548efba0d2a45269b623b1b15
Bdosborn <B.d.osborn@gmail.com>
2021-11-28 17:50:26 +0000

    datetime_examples update

--- DATE and TIME data types/DateTime_Examples.sql
+++ DATE and TIME data types/DateTime_Examples.sql
@@ -3,19 +3,20 @@
 -- DROP TABLE [dbo].[ORDERS]
 
 CREATE TABLE [dbo].[ORDERS] (
-     [order_number]         AS INT NOT NULL
-    ,[order_datetime]       AS DATETIME NULL
-    ,[order_date]           AS DATE NULL
-    ,[delivery_datetime]    AS DATETIME NULL
+     [order_number]         INT NOT NULL
+    ,[order_datetime]       DATETIME NULL
+    ,[order_date]           DATE NULL
+    ,[delivery_datetime]    DATETIME NULL
 )
 
 INSERT INTO [dbo].[ORDERS] ([order_number], [order_datetime], [order_date], [delivery_datetime])
 VALUES  
-    ('1','', '', ''),
-    ('1','', '', ''),
-    ('1','', '', ''),
-    ('1','', '', ''),
-    ('1','', '', '')
+    ('1','2021-10-01 10:30:00', '2021-10-01', '2021-10-12 10:11:10'),
+    ('2','2021-10-01 12:45:00', '2021-10-01', '2021-10-10 14:10:11'),
+    ('3','2021-10-30 13:25:00', '2021-10-30', '2021-11-05 13:22:12'),
+    ('4','2021-09-22 16:30:00', '2021-09-22', '2021-09-25 14:12:13'),
+    ('5','2021-01-10 18:15:00', '2021-01-10', '2021-02-01 11:15:14'),
+    ('6',CURRENT_TIMESTAMP, CONVERT(DATE, CURRENT_TIMESTAMP), DATEADD(DAY, 5, CURRENT_TIMESTAMP))
 
 
 
@@ -131,7 +132,7 @@ SELECT
 SELECT  
     DATEADD(DAY, 1, EOMONTH('2021-10-10', -1)) 'month_start'
 
---
+-- return the orders between the start and end of the month of october
 SELECT
      [Order_ID]
     ,[Order_Date]
@@ -140,6 +141,36 @@ FROM
 WHERE
     [order_date] BETWEEN EOMONTH('2021-10-10') AND DATEADD(DAY, 1, EOMONTH('2021-10-10', -1))
 
+
+
+DECLARE @MyDate AS DATETIME
+SET @MyDate = '2021-10-30'
+
+SELECT DATEDIFF(month, 0, @mydate) 'days_since_zero'
+
+SELECT DATEADD(month, 1461, 0) 'zero_plus_1461_months'
+
+SELECT DATEADD(month, DATEDIFF(month, 0, @mydate), 0) 'month_start'
+
+-- show all orders where the order_date is between the first and the last date of our date variable
+SELECT     
+     [Order_ID]
+    ,[Order_Date]
+FROM	[ORDERS]
+WHERE	[order_date] BETWEEN     
+                        DATEADD(month,  DATEDIFF(month, 0, @mydate), 0)
+                        AND DATEADD(day,-1, DATEADD(month, DATEDIFF(month, -1, @mydate), 0))
+
+
+
+-- show all orders where the order_date is within the current month
+SELECT     
+     [Order_ID]
+    ,[Order_Date]
+FROM	[dbo].[ORDERS]
+WHERE	[order_date] BETWEEN     
+                        DATEADD(month,  DATEDIFF(month, 0, CURRENT_TIMESTAMP), 0)
+                        AND DATEADD(day,-1, DATEADD(month, DATEDIFF(month, -1, CURRENT_TIMESTAMP), 0))
 
 
 
